0b6292265e9b3c830a86f1bbc4c8a73c25e22c0c
Chris Kastorff <encryptio@gmail.com>
2010-07-14 16:25:16 -0600

    lots more utility functions

--- util.c
+++ util.c
@@ -5,6 +5,42 @@
 
 #include "util.h"
 
+////////////////////////////////////////////////////////////////////////////////
+// int le (2s complement)
+
+int64_t read_le_int64(int fh, off_t offset) {
+    uint8_t buf[8];
+    lseek(fh, offset, SEEK_SET);
+    read(fh, buf, 8);
+    int64_t ret = ((((((((((((((int64_t)(buf[7] & 0x7F) << 8) + buf[6]) << 8) + buf[5]) << 8) + buf[4]) << 8) + buf[3]) << 8) + buf[2]) << 8) + buf[1]) << 8) + buf[0];
+    if ( buf[7] & 0x80 )
+        ret = ret - ((int64_t) 1 << 63) - ((int64_t) 1 << 63);
+    return ret;
+}
+
+int32_t read_le_int32(int fh, off_t offset) {
+    uint8_t buf[4];
+    lseek(fh, offset, SEEK_SET);
+    read(fh, buf, 4);
+    int32_t ret = (((((((int32_t)(buf[3] & 0x7F)) << 8) + buf[2]) << 8) + buf[1]) << 8) + buf[0];
+    if ( buf[3] & 0x80 )
+        ret = ret - ((int32_t) 1 << 31) - ((int32_t) 1 << 31);
+    return ret;
+}
+
+int16_t read_le_int16(int fh, off_t offset) {
+    uint8_t buf[2];
+    lseek(fh, offset, SEEK_SET);
+    read(fh, buf, 2);
+    int16_t ret = (((int16_t) (buf[1] & 0x7F)) << 8) + buf[0];
+    if ( buf[1] & 0x80 )
+        ret = ret - ((int16_t) 1 << 15) - ((int16_t) 1 << 15);
+    return ret;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// uint le
+
 uint64_t read_le_uint64(int fh, off_t offset) {
     uint8_t buf[8];
     lseek(fh, offset, SEEK_SET);
@@ -26,6 +62,9 @@ uint16_t read_le_uint16(int fh, off_t offset) {
     return (((uint16_t)buf[1]) << 8) + buf[0];
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// uint be
+
 uint64_t read_be_uint64(int fh, off_t offset) {
     uint8_t buf[8];
     lseek(fh, offset, SEEK_SET);
@@ -46,4 +85,25 @@ uint16_t read_be_uint16(int fh, off_t offset) {
     read(fh, buf, 2);
     return (((uint16_t)buf[0]) << 8) + buf[1];
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// uint8
+
+uint8_t read_uint8(int fh, off_t offset) {
+    uint8_t buf;
+    lseek(fh, offset, SEEK_SET);
+    read(fh, &buf, 1);
+    return buf;
+}
+
+int8_t read_int8(int fh, off_t offset) { // 2s complement
+    uint8_t buf;
+    lseek(fh, offset, SEEK_SET);
+    read(fh, &buf, 1);
+    int8_t ret = buf & 0x7F;
+    if ( buf & 0x80 )
+        ret = ret - ((int8_t) 1 << 7) - ((int8_t) 1 << 7);
+    return ret;
+}
+
 
--- util.h
+++ util.h
@@ -4,12 +4,18 @@
 #include <inttypes.h>
 #include <fcntl.h>
 
+int64_t read_le_int64(int fh, off_t offset);
+int32_t read_le_int32(int fh, off_t offset);
+int16_t read_le_int16(int fh, off_t offset);
+int8_t  read_int8(    int fh, off_t offset);
+
 uint64_t read_le_uint64(int fh, off_t offset);
 uint64_t read_be_uint64(int fh, off_t offset);
 uint32_t read_le_uint32(int fh, off_t offset);
 uint32_t read_be_uint32(int fh, off_t offset);
 uint16_t read_le_uint16(int fh, off_t offset);
 uint16_t read_be_uint16(int fh, off_t offset);
+uint8_t  read_uint8(    int fh, off_t offset);
 
 #endif
 
