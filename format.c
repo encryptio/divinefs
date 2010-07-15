76e57c80a210ff654410c218372a0c72c5158b8f
Chris Kastorff <encryptio@gmail.com>
2010-07-15 16:30:58 -0400

    remove format_bsdlabel

--- format.c
+++ format.c
@@ -11,7 +11,6 @@
 #define FORMAT_NBUFS 32
 #define FORMAT_OFFSET_LENGTH 64
 #define FORMAT_UUID_LENGTH 37
-#define FORMAT_BSDLABEL_LENGTH 200
 #define FORMAT_HUMANSIZE_LENGTH 20
 
 static char format_offset_buffers[FORMAT_NBUFS][FORMAT_OFFSET_LENGTH];
@@ -56,30 +55,6 @@ char *format_uuid(exec_options *eo, uint8_t *uuid) {
             uuid[6], uuid[7],
             uuid[8], uuid[9],
             uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]);
-
-    return ret;
-}
-
-static char format_bsdlabel_buffers[FORMAT_NBUFS][FORMAT_BSDLABEL_LENGTH];
-static int format_bsdlabel_which = 0;
-
-char *format_bsdlabel(exec_options *eo, char *type, off_t offset, off_t size, char *mountpoint) {
-    char *ret = format_bsdlabel_buffers[format_uuid_which++];
-    if ( format_bsdlabel_which >= FORMAT_NBUFS )
-        format_bsdlabel_which = 0;
-
-    if ( strlen(mountpoint) )
-        snprintf(ret, FORMAT_BSDLABEL_LENGTH,
-                "  X: % 13llu % 13llu % 7s                   # %s\n",
-                (long long unsigned int) size/512,
-                (long long unsigned int) offset/512,
-                type, mountpoint);
-    else
-        snprintf(ret, FORMAT_BSDLABEL_LENGTH,
-                "  X: % 13llu % 13llu % 7s\n",
-                (long long unsigned int) size/512,
-                (long long unsigned int) offset/512,
-                type);
 
     return ret;
 }
--- format.h
+++ format.h
@@ -8,7 +8,6 @@
 
 char *format_offset(exec_options *eo, off_t offset);
 char *format_uuid(exec_options *eo, uint8_t *uuid);
-char *format_bsdlabel(exec_options *eo, char *type, off_t offset, off_t size, char *mountpoint);
 char *format_humansize(exec_options *eo, off_t size);
 
 #endif
